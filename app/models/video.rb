40e9c2e4aeabaec3a8e7a00d346e7c2d8c13b877
Vinoth <a.vinothk@outlook.com>
2016-03-07 23:15:09 +0530

    Save video meta information

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,13 @@
 class Video < ActiveRecord::Base
+  before_save :update_meta_info
+
+  private
+
+  def update_meta_info
+    video = Yt::Video.new url: self.url
+    self.title = video.title
+    self.description = video.description
+    self.tags = video.tags
+  end
+
 end
