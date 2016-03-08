3c77e048ea8b8d5ae53c713ec6fc647eed6881af
Vinoth <a.vinothk@outlook.com>
2016-03-08 21:50:36 +0530

    Style Videos index and show page

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,13 +1,27 @@
 class Video < ActiveRecord::Base
   before_save :update_meta_info
 
+  def embed
+    video = yt_video
+    video.embed_html
+  end
+
+  def thumbnail
+    video = yt_video
+    video.thumbnail_url
+  end
+
   private
 
   def update_meta_info
-    video = Yt::Video.new url: self.url
+    video = yt_video
     self.title = video.title
     self.description = video.description
     self.tags = video.tags
+  end
+
+  def yt_video
+    Yt::Video.new url: self.url
   end
 
 end
