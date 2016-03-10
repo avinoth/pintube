c4e5fd48aa85fc55cebb153e81b8d39015494ce7
Vinoth <a.vinothk@outlook.com>
2016-03-10 06:43:51 +0530

    Add boards scaffold

--- spec/requests/videos_spec.rb
+++ spec/requests/videos_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "Videos", type: :request do
+RSpec.describe "Videos" do
   describe "GET /videos" do
-    it "works! (now write some real specs)" do
+    it "should render videos index page" do
       get videos_path
       expect(response).to have_http_status(200)
+      expect(response).to render_template(:index)
     end
   end
+
+  describe "GET /videos/new" do
+    it "should render videos form" do
+      get new_video_path
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:new)
+    end
 end
